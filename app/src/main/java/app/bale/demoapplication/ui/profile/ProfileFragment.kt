5b0cb731806add4f0500c47793d3f1aa78465fe0
Prashant <bale.prashant1984@gmail.com>
2022-03-05 17:50:40 +0530

    Code/UI refactoring

--- app/src/main/java/app/bale/demoapplication/ui/profile/ProfileFragment.kt
+++ app/src/main/java/app/bale/demoapplication/ui/profile/ProfileFragment.kt
@@ -1,13 +1,17 @@
 package app.bale.demoapplication.ui.profile
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import app.bale.demoapplication.databinding.FragmentProfileBinding
+
 
 class ProfileFragment : Fragment() {
 
@@ -26,11 +30,62 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        profileViewModel.text.observe(viewLifecycleOwner, {
-            textView.text = it
-        })
+        setDetailsAboutMe()
+
         return root
+    }
+
+    private fun setDetailsAboutMe() {
+        binding.callBtn.setOnClickListener {
+            val phone = "+918530669827"
+            val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null))
+            startActivity(intent)
+        }
+
+        binding.emailBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf<String>("prashant.bale@hotmail.com"))
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Hello...!!")
+            intent.putExtra(Intent.EXTRA_TEXT, "")
+            intent.data = Uri.parse("mailto:")
+
+            if (activity?.packageManager?.let { it1 -> intent.resolveActivity(it1) } != null) {
+                startActivity(intent)
+            } else {
+                Toast.makeText(
+                    activity, "There is no application that support this action",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        binding.linkediBtn.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.linkedin.com/profile/view?id=prashantbale"))
+            val packageManager = context?.packageManager
+            val list =
+                packageManager?.queryIntentActivities(intent!!, PackageManager.MATCH_DEFAULT_ONLY)
+            if (list?.isEmpty() == true) {
+                intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("http://www.linkedin.com/profile/view?id=prashantbale")
+                )
+            }
+            startActivity(intent)
+        }
+
+        binding.githubBtn.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/prbale/"))
+            val packageManager = context?.packageManager
+            val list =
+                packageManager?.queryIntentActivities(intent!!, PackageManager.MATCH_DEFAULT_ONLY)
+            if (list?.isEmpty() == true) {
+                intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://github.com/prbale/")
+                )
+            }
+            startActivity(intent)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/app/bale/demoapplication/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/app/bale/demoapplication/ui/profile/ProfileViewModel.kt
@@ -1,13 +1,5 @@
 package app.bale.demoapplication.ui.profile
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ProfileViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "Prashant Bale\nMobile Architect"
-    }
-    val text: LiveData<String> = _text
-}
+class ProfileViewModel : ViewModel()
