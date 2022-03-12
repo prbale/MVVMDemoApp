61d337575e212b1c1b26dc73cee6a82e8c64f1fc
Prashant <bale.prashant1984@gmail.com>
2022-03-12 18:06:58 +0530

    REfactoring

--- app/src/main/java/app/bale/demoapplication/constants/AppConstants.kt
+++ app/src/main/java/app/bale/demoapplication/constants/AppConstants.kt
@@ -3,8 +3,8 @@ package app.bale.demoapplication.constants
 // Service Related Constants
 object AppConstants {
 
-    const val PHONE_NUMBER      = "+918530669827"
-    const val EMAIL             = "prashant.bale@hotmail.com"
-    const val LINKED_IN_PROFILE = "http://www.linkedin.com/profile/view?id=prashantbale"
+    const val PHONE_NUMBER      = ""
+    const val EMAIL             = ""
+    const val LINKED_IN_PROFILE = ""
     const val GITHUB_PROFILE    = "https://github.com/prbale/"
 }
